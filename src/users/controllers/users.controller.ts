c7d7c2049e97d0104e539acddbb8ae9aa7bbcd4e
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-08 20:31:34 +0900

    Send 201(created) when user is created instead of 200(success).

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -19,7 +19,7 @@ class UsersController {
   async createUser(req: express.Request, res: express.Response){
     req.body.password = await argon2.hash(req.body.password);
     const userId = await userService.create(req.body);
-    res.status(200).send({ id: userId });
+    res.status(201).send({ id: userId });
   }
 
   async patch(req: express.Request, res: express.Response) {
