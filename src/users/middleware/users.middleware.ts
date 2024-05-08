3c4921140a26d747b663f2a03fcf90a9d7d956a6
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-08 10:50:30 +0900

    Fix typo.

--- src/users/middleware/users.middleware.ts
+++ src/users/middleware/users.middleware.ts
@@ -50,7 +50,7 @@ class UsersMiddleware {
 
   async userCantChangePermission(req: express.Request, res: express.Response, next: express.NextFunction) {
     if ('permissionFlags' in req.body && req.body.permissionFlags !== res.locals.user.permissionFlags) {
-      res.status(400).send({ errors: ['User cannot changer permission flags'] });
+      res.status(400).send({ errors: ['User cannot change permission flags'] });
     } else {
       next();
     }
