37617d1c7c383b71b509b8eef08e638621558ab2
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-24 17:29:43 +0900

    Add nodemon.

--- dist/src/messages/services/twilio.service.js
+++ dist/src/messages/services/twilio.service.js
@@ -8,15 +8,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const logger_1 = __importDefault(require("../../lib/logger"));
 class TwilioService {
     sendMessage(recipients, messageBody) {
         return __awaiter(this, void 0, void 0, function* () {
             const recipientsArray = recipients.split(' ');
-            //call Twilio API here
+            //TODO call Twilio API here
+            //TODO Log to an actual logfile
+            logger_1.default.info('Twilio');
+            logger_1.default.info(recipientsArray);
+            logger_1.default.info(messageBody);
             return 'Success!';
         });
     }
 }
 exports.default = new TwilioService();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHdpbGlvLnNlcnZpY2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvbWVzc2FnZXMvc2VydmljZXMvdHdpbGlvLnNlcnZpY2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7QUFBQSxNQUFNLGFBQWE7SUFHWCxXQUFXLENBQUMsVUFBa0IsRUFBRSxXQUFtQjs7WUFDdkQsTUFBTSxlQUFlLEdBQUcsVUFBVSxDQUFDLEtBQUssQ0FBQyxHQUFHLENBQUMsQ0FBQztZQUM5QyxzQkFBc0I7WUFDdEIsT0FBTyxVQUFVLENBQUE7UUFDbkIsQ0FBQztLQUFBO0NBQ0Y7QUFFRCxrQkFBZSxJQUFJLGFBQWEsRUFBRSxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHdpbGlvLnNlcnZpY2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvbWVzc2FnZXMvc2VydmljZXMvdHdpbGlvLnNlcnZpY2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7QUFBQSw4REFBc0M7QUFFdEMsTUFBTSxhQUFhO0lBR1gsV0FBVyxDQUFDLFVBQWtCLEVBQUUsV0FBbUI7O1lBQ3ZELE1BQU0sZUFBZSxHQUFHLFVBQVUsQ0FBQyxLQUFLLENBQUMsR0FBRyxDQUFDLENBQUM7WUFDOUMsMkJBQTJCO1lBQzNCLCtCQUErQjtZQUMvQixnQkFBTSxDQUFDLElBQUksQ0FBQyxRQUFRLENBQUMsQ0FBQTtZQUNyQixnQkFBTSxDQUFDLElBQUksQ0FBQyxlQUFlLENBQUMsQ0FBQztZQUM3QixnQkFBTSxDQUFDLElBQUksQ0FBQyxXQUFXLENBQUMsQ0FBQztZQUN6QixPQUFPLFVBQVUsQ0FBQTtRQUNuQixDQUFDO0tBQUE7Q0FDRjtBQUVELGtCQUFlLElBQUksYUFBYSxFQUFFLENBQUMifQ==
