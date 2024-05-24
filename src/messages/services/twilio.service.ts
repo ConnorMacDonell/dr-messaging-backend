303c3a946158b0f81acf3b1fa54b8da3ee93eac8
Connor MacDonell <connor.macdonell1@gmail.com>
2024-05-24 17:28:07 +0900

    Fix typo in messagesMiddleware.

--- src/messages/services/twilio.service.ts
+++ src/messages/services/twilio.service.ts
@@ -1,9 +1,15 @@
+import Logger from "../../lib/logger";
+
 class TwilioService {
 
 
   async sendMessage(recipients: string, messageBody: string){
     const recipientsArray = recipients.split(' ');
-    //call Twilio API here
+    //TODO call Twilio API here
+    //TODO Log to an actual logfile
+    Logger.info('Twilio')
+    Logger.info(recipientsArray);
+    Logger.info(messageBody);
     return 'Success!'
   }
 }
